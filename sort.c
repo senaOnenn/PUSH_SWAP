bb9d9ef67efcb5a4cf15431fe447eb26fd0cc0bd
Emine Sena" Önen <eonen@k1m35s07.42istanbul.com.tr>
2025-08-22 19:40:11 +0300

    updated

--- sort.c
+++ sort.c
@@ -6,9 +6,21 @@
 /*   By: eonen <eonen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 11:57:32 by eonen             #+#    #+#             */
-/*   Updated: 2025/08/22 18:51:57 by eonen            ###   ########.fr       */
+/*   Updated: 2025/08/22 19:40:03 by eonen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap"
 
+int	find_max_bits(t_stack *stack)
+{
+	int max;
+
+	max = 0;
+	while (stack) // stackin sonuna kadar gez
+	{
+		if (stack->index > max)
+			max = stack->index; //en büyük indeksi bul
+        stack = stack-> next;
+	}
+}
